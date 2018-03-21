71c3ff0b60608d01472143ee1884e5e8c3d42160
mka <mka281@icloud.com>
2018-03-21 19:02:58 +0300

    Add MongoDB

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,38 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
-passport.use(new GoogleStrategy({
+passport.serializeUser((user, done) => {
+  done(null, user.id); // Use user.id because there may be other oauth options
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
+
+passport.use(new GoogleStrategy(
+  {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
-  }, (accessToken, refreshToken, profile, done) => {
-      console.log("ACCES TOKEN", accessToken);
-      console.log("REFRESH TOKEN", refreshToken);
-      console.log("PROFILE", profile);
-    }
-  )
-)
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id })
+      .then( (existingUser) => {
+        // Save the user if it does not exist on our db
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      })
+  }
+));
