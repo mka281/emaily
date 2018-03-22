a8a4d67168ff466761926ca4a283b3e9e09f5ed6
mka <mka281@icloud.com>
2018-03-22 21:48:26 +0300

    Add action creator

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -8,9 +11,9 @@ const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
-
+    this.props.fetchUser();
   }
-  
+
   render() {
     return(
       <div className="container">
@@ -27,4 +30,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
