8828c35bdebc4098ac5be4278ac0c93ea7bf3831
mka <mka281@icloud.com>
2018-03-24 12:40:29 +0300

    Create new middleware to check credits

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,6 @@
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 
 module.exports = app => {
-  app.post('/api/survey', requireLogin,  (req, res) => {});
+  app.post('/api/survey', requireLogin, requireCredits, (req, res) => {});
 };
